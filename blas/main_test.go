aea4aed532f745b54e173e16245d80a7cc755ee0
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-21 15:35:15 +0200

    Changed the formatting of main_test

--- blas/main_test.go
+++ blas/main_test.go
@@ -1,37 +1,37 @@
 package blas
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestDgemm(t *testing.T) {
-  m := 2
-  n := 4
-  k := 3
+	m := 2
+	n := 4
+	k := 3
 
-  a := []float64{1, 4, 2, 5, 3, 6}
-  b := []float64{1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12}
-  c := []float64{2, 7, 6, 2, 0, 7, 4, 2}
+	a := []float64{1, 4, 2, 5, 3, 6}
+	b := []float64{1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12}
+	c := []float64{2, 7, 6, 2, 0, 7, 4, 2}
 
-  Dgemm('n', 'n', m, n, k, 1, a, m, b, k, 1, c, m)
+	Dgemm('n', 'n', m, n, k, 1, a, m, b, k, 1, c, m)
 
-  d := []float64{40, 90, 50, 100, 50, 120, 60, 130}
+	d := []float64{40, 90, 50, 100, 50, 120, 60, 130}
 
-  for i := range c {
-    if c[i] != d[i] {
-      t.Error("The result is incorrect.")
-    }
-  }
+	for i := range c {
+		if c[i] != d[i] {
+			t.Error("The result is incorrect.")
+		}
+	}
 }
 
 func BenchmarkDgemm(t *testing.B) {
-  m := 1000
+	m := 1000
 
-  a := make([]float64, m * m)
-  b := make([]float64, m * m)
-  c := make([]float64, m * m)
+	a := make([]float64, m*m)
+	b := make([]float64, m*m)
+	c := make([]float64, m*m)
 
-  for i := 0; i < t.N; i++ {
-    Dgemm('n', 'n', m, m, m, 1, a, m, b, m, 1, c, m)
-  }
+	for i := 0; i < t.N; i++ {
+		Dgemm('n', 'n', m, m, m, 1, a, m, b, m, 1, c, m)
+	}
 }

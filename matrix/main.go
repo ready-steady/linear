a3c7777c25b75eb9a127648bdd05a423bec9eb2f
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-21 08:32:09 +0200

    Moved matrix into a separate package

--- matrix/main.go
+++ matrix/main.go
@@ -1,14 +1,23 @@
-package linear
+package matrix
 
 import (
   "errors"
 )
 
-type float float64
-
 type Matrix struct {
-  rows, cols uint
-  data []float
+  rows, cols int
+  data []float64
+}
+
+func New(rows, cols int, data []float64) (Matrix, error) {
+  switch len(data) {
+  case 0:
+    return Matrix{rows, cols, make([]float64, rows * cols)}, nil
+  case rows * cols:
+    return Matrix{rows, cols, data}, nil
+  default:
+    return Matrix{}, errors.New("The data are of an invalid length.")
+  }
 }
 
 func (A *Matrix) Equal(B *Matrix) bool {
@@ -41,10 +50,10 @@ func MatrixAdd(A, B *Matrix) (Matrix, error) {
   m, n := A.rows, A.cols
 
   if m != B.rows || n != B.cols {
-    return Matrix{}, errors.New("B should have the same dimensions as A.")
+    return Matrix{}, errors.New("A should have the same dimensions as B.")
   }
 
-  R := Matrix{ rows: m, cols: n, data: make([]float, m * n) }
+  R := Matrix{ rows: m, cols: n, data: make([]float64, m * n) }
 
   for i := range A.data {
     R.data[i] = A.data[i] + B.data[i]
@@ -57,22 +66,18 @@ func MatrixMultiply(A, B *Matrix) (Matrix, error) {
   m, n, p := A.rows, A.cols, B.cols
 
   if n != B.rows {
-    return Matrix{}, errors.New("B should have as many rows as A has columns.")
+    return Matrix{}, errors.New("A should have as many columns as B has rows.")
   }
 
-  R := Matrix{ rows: m, cols: p, data: make([]float, m * p) }
+  R := Matrix{ rows: m, cols: p, data: make([]float64, m * p) }
 
-  var i, j, k uint
-  var sum float
-
-  for i = 0; i < m; i++ {
-    for j = 0; j < p; j++ {
+  var sum float64
+  for i := 0; i < m; i++ {
+    for j := 0; j < p; j++ {
       sum = 0
-
-      for k = 0; k < n; k++ {
+      for k := 0; k < n; k++ {
         sum += A.data[i * n + k] * B.data[k * p + j]
       }
-
       R.data[i * p + j] = sum
     }
   }
--- matrix/main_test.go
+++ matrix/main_test.go
@@ -1,11 +1,11 @@
-package linear
+package matrix
 
 import "testing"
 
 func TestMatrixAdd(t *testing.T) {
-  A := Matrix{ 3, 3, []float{0, 1, 2, 3, 4, 3, 2, 1, 0} }
-  B := Matrix{ 3, 3, []float{1, 2, 3, 4, 5, 6, 7, 8, 9} }
-  C := Matrix{ 3, 3, []float{1, 3, 5, 7, 9, 9, 9, 9, 9} }
+  A, _ := New(3, 3, []float64{0, 1, 2, 3, 4, 3, 2, 1, 0})
+  B, _ := New(3, 3, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
+  C, _ := New(3, 3, []float64{1, 3, 5, 7, 9, 9, 9, 9, 9})
 
   R, err := A.Add(&B)
 
@@ -19,8 +19,8 @@ func TestMatrixAdd(t *testing.T) {
 }
 
 func TestMatrixAddError(t *testing.T) {
-  A := Matrix{ 3, 3, []float{0, 1, 2, 3, 4, 3, 2, 1, 0} }
-  B := Matrix{ 2, 3, []float{1, 2, 3, 4, 5, 6} }
+  A, _ := New(3, 3, []float64{0, 1, 2, 3, 4, 3, 2, 1, 0})
+  B, _ := New(2, 3, []float64{1, 2, 3, 4, 5, 6})
 
   R, err := A.Add(&B)
 
@@ -34,9 +34,9 @@ func TestMatrixAddError(t *testing.T) {
 }
 
 func TestMatrixMultiply(t *testing.T) {
-  A := Matrix{ 3, 4, []float{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12} }
-  B := Matrix{ 4, 2, []float{1, 2, 3, 4, 5, 6, 7, 8} }
-  C := Matrix{ 3, 2, []float{50, 60, 114, 140, 178, 220} }
+  A, _ := New(3, 4, []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+  B, _ := New(4, 2, []float64{1, 2, 3, 4, 5, 6, 7, 8})
+  C, _ := New(3, 2, []float64{50, 60, 114, 140, 178, 220})
 
   R, err := A.Multiply(&B)
 

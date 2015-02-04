09b38999042071d5133eea36f119a7601bb7ac15
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-04 10:27:48 +0100

    Added Uniform to metric

--- metric/main.go
+++ metric/main.go
@@ -5,8 +5,7 @@ import (
 	"math"
 )
 
-// L2 computes the Euclidean distance between two n-element vectors in the
-// n-dimensional Euclidean space.
+// L2 computes the Euclidean distance between two n-element vectors.
 func L2(x, y []float64) float64 {
 	var sum, Δ float64
 
@@ -16,4 +15,21 @@ func L2(x, y []float64) float64 {
 	}
 
 	return math.Sqrt(sum)
+}
+
+// Uniform computes the uniform distance between two n-element vectors.
+func Uniform(x, y []float64) float64 {
+	var δ, Δ float64
+
+	for i := range x {
+		δ = x[i] - y[i]
+		if δ < 0 {
+			δ = -δ
+		}
+		if δ > Δ {
+			Δ = δ
+		}
+	}
+
+	return Δ
 }

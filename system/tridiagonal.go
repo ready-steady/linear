3d75d2b8dbae86f3a88a495c8fd0847221edb0c9
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-27 14:11:52 +0100

    Extended system.ComputeTriangular to multiple right-hand sides

--- system/tridiagonal.go
+++ system/tridiagonal.go
@@ -8,26 +8,33 @@ package system
 //     a(i-1)*x(i-1) + b(  i)*x(  i) + c(i+1)*x(i+1) = d(  i), for i = 1, ..., (n-2),
 //     a(n-2)*x(n-2) + b(n-1)*x(n-1)                 = d(n-1)
 //
-// Note that a(n-1) and c(0) are not involved in the system.
+// Note that a(n-1) and c(0) are not involved in the system. If d is a matrix,
+// the system is solved for each column of that matrix.
 func ComputeTridiagonal(a, b, c, d []float64) []float64 {
-	n := len(d)
+	n := len(b)
+	nd := len(d) / n
 
+	bp := make([]float64, n-1)
 	cp := make([]float64, n-1)
 	cp[0] = c[1] / b[0]
+	bp[0] = b[1] - a[0]*cp[0]
 	for i := 1; i < (n - 1); i++ {
-		cp[i] = c[i+1] / (b[i] - a[i-1]*cp[i-1])
+		cp[i] = c[i+1] / bp[i-1]
+		bp[i] = b[i+1] - a[i]*cp[i]
 	}
 
 	dp := make([]float64, n)
-	dp[0] = d[0] / b[0]
-	for i := 1; i < n; i++ {
-		dp[i] = (d[i] - a[i-1]*dp[i-1]) / (b[i] - a[i-1]*cp[i-1])
-	}
+	x := make([]float64, n*nd)
+	for i := 0; i < nd; i++ {
+		dp[0] = d[i*n] / b[0]
+		for j := 1; j < n; j++ {
+			dp[j] = (d[i*n+j] - a[j-1]*dp[j-1]) / bp[j-1]
+		}
 
-	x := make([]float64, n)
-	x[n-1] = dp[n-1]
-	for i := n - 2; i >= 0; i-- {
-		x[i] = dp[i] - cp[i]*x[i+1]
+		x[i*n+n-1] = dp[n-1]
+		for j := n - 2; j >= 0; j-- {
+			x[i*n+j] = dp[j] - cp[j]*x[i*n+j+1]
+		}
 	}
 
 	return x

af1ac49feec923afa285d86334614993780e1c11
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-12 13:34:06 +0200

    metric: make a couple of stylistic adjustments

--- metric/main.go
+++ metric/main.go
@@ -7,31 +7,27 @@ import (
 	"math"
 )
 
-// L2 computes the Euclidean distance between two n-element vectors.
+// L2 computes the Euclidean distance between two vectors.
 func L2(x, y []float64) float64 {
-	var sum, Δ float64
-
+	sum := 0.0
 	for i := range x {
-		Δ = x[i] - y[i]
+		Δ := x[i] - y[i]
 		sum += Δ * Δ
 	}
-
 	return math.Sqrt(sum)
 }
 
-// Uniform computes the uniform distance between two n-element vectors.
+// Uniform computes the uniform distance between two vectors.
 func Uniform(x, y []float64) float64 {
-	var δ, Δ float64
-
+	Δ := 0.0
 	for i := range x {
-		δ = x[i] - y[i]
-		if δ < 0 {
+		δ := x[i] - y[i]
+		if δ < 0.0 {
 			δ = -δ
 		}
 		if δ > Δ {
 			Δ = δ
 		}
 	}
-
 	return Δ
 }
